8299c045dacb263ff77afbdd976c44b931eddaac
Nuttapol Nonseeveerachai <nuttapol.nonsee@gmail.com>
2017-01-14 21:35:59 +0700

    Create a task to concat vendor js and css files.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')
 var browserSync = require('browser-sync')
+var concat = require('gulp-concat')
 
 // SCSS dev path
 const scssDevPath = "./app/scss/**/*.scss"
@@ -10,7 +11,7 @@ const scssDevPath = "./app/scss/**/*.scss"
 const htmlClientDevPath	=	"./app/html/**/*.html"
 
 // Default gulp task when hit "gulp" in cli.
-gulp.task('default', ['sass', 'html-to-public'], function () {
+gulp.task('default', ['sass', 'html-to-public', 'concat-vendor-js', 'concat-vendor-css'], function () {
 
 	// // เมื่อไฟล์ html หรือ css มีการเปลี่ยนแปลง ก็ให้รีเฟรช web browser
 	// gulp.watch(['**/*.html'], browserSync.reload)
@@ -47,5 +48,24 @@ gulp.task('html-to-public', function () {
 	return gulp.src(htmlClientDevPath, {
 		base: './app/html'
 	})
-	.pipe(gulp.dest('./public/html/'))
+	.pipe(gulp.dest('./public/'))
+})
+
+// Create a task to concatenate vendor script.
+gulp.task('concat-vendor-js', function() {
+  return gulp.src([
+		'./bower_components/jquery/dist/jquery.min.js', 
+		'./bower_components/materialize/dist/js/materialize.min.js'
+	])
+	.pipe(concat('vendors.js'))
+	.pipe(gulp.dest('./public/js/'))
+})
+
+// Create a task to concatenate vendor css.
+gulp.task('concat-vendor-css', function() {
+  return gulp.src([
+		'./bower_components/materialize/dist/css/materialize.min.css'
+	])
+	.pipe(concat('vendors.css'))
+	.pipe(gulp.dest('./public/css/'))
 })
