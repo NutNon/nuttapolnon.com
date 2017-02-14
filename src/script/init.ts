5e48a20d394d3717bfabd69d7a950626bf4eff6e
Nuttapol Nonseeveerachai <nuttapol.nonsee@gmail.com>
2017-02-15 02:48:28 +0700

    Add contents to works section and also init swiper for work slides.

--- src/script/init.ts
+++ src/script/init.ts
@@ -2,4 +2,13 @@ $(function () {
 	$('.scrollspy').scrollSpy({
 		scrollOffset: 64
 	})
+
+	var mySwiper = new Swiper ('.swiper-container', {		
+		// Navigation arrows
+		nextButton: '.swiper-button-next',
+		prevButton: '.swiper-button-prev',
+		parallax: true,
+		speed: 600,
+		// effect: 'fade' ** caused previou slide to stay behind.
+	})        
 })
